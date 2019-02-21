43dcbd44be64d52916b2b03abd8fc7a109ff5364
Ivan-Grebennikov <gre-ivan@yandex.ru>
2019-02-21 20:12:00 +0300

    [update] changes in init.sh + add gitignore to empty dirs

--- init.sh
+++ init.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-APP_DIR="$(cd "${BASH_SOURCE%/*}" && pwd)"
-
-sudo ln -fs "${APP_DIR}/etc/nginx.conf" /etc/nginx/sites-enabled/stepic-web-app.conf
+sudo ln -fs /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo service nginx restart
